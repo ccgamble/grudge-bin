a1e159bff334efbffba0dd9a6bf87b6c33a13868
ccgamble <christine.c.gamble@gmail.com>
2017-02-24 02:48:50 -0700

    Fixes syntax error in server.js

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
 
-
 app.locals.grudges = []
 
 app.get('/', (request, response) => {
